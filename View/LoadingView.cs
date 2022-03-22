af7b2e775505084b72e9419fbcb6e22bf7c5c1a0
Maicon Oliveira <96441568+maicxn@users.noreply.github.com>
2022-03-21 22:14:20 -0300

    Add files via upload

---
+++ View/LoadingView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Terça_Feira.View
+{
+    internal sealed partial class LoadingView : Form
+    {
+        internal LoadingView()
+        {
+            InitializeComponent();
+            Text = 
+        }
+    }
+}
