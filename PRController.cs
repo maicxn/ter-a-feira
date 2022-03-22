e3f82b7cf1d51a6804a6a3a06d15ded9bafdc8a4
Maicon Oliveira <96441568+maicxn@users.noreply.github.com>
2022-03-21 22:09:37 -0300

    Add files via upload

---
+++ PRController.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Terça_Feira
+{
+    internal static class PRController
+    {
+        internal static string Name => Properties.Settings.Default.Name;
+        internal static float Confidence => Properties.Settings.Default.Confidence;
+
+        internal static bool DebugMode
+        {
+
+            get { return Properties.Settings.Default.DebugMode; }
+            set { Properties.Settings.Default.DebugMode = value; }
+        }
+
+        internal static void Save() => Properties.Settings.Default.Save();
+    }
+}
