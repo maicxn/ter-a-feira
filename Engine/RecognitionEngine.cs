5920247b2a3e0156b6235567722d9a1f3ea25388
Maicon Oliveira <96441568+maicxn@users.noreply.github.com>
2022-03-21 22:13:06 -0300

    Add files via upload

---
+++ Engine/RecognitionEngine.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Speech.Recognition;
+
+using System.Globalization;
+using Terça_Feira.Grammatics;
+
+namespace Terça_Feira.Engine
+{
+    internal sealed class RecognitionEngine : SpeechRecognitionEngine
+    {
+        internal RecognitionEngine(CultureInfo cultureInfo) : base(cultureInfo)
+        {
+            SetInputToDefaultAudioDevice();
+            LoadGrammar();
+
+        }
+
+        internal bool IsRunning { get; private set; } = false;
+
+        internal void Start()
+        {
+            if (!IsRunning)
+                RecognizeAsync(RecognizeMode.Multiple);
+            IsRunning = true;
+        }
+
+        internal void Stop()
+        {
+
+            if (IsRunning)
+                RecognizeAsyncCancel();
+            IsRunning = false;
+        }
+
+        private void LoadGrammar()
+        {
+            foreach (var g in Builder.GetGrammars())
+            {
+                LoadGrammar(g);
+            }
+        }
+
+
+    }
+
+}
